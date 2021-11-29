db9c55965cbd5075bd90eddbfc01bcbd9637c992
Jared <jaredgridley.mldl@gmail.com>
2021-11-29 16:23:27 -0500

    data download script

--- setup_scripts/get_data.py
+++ setup_scripts/get_data.py
@@ -4,13 +4,41 @@
 # get_data.py
 #
 
-def celebA_drive():
-    file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
-    request = drive_service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print
-        "Download %d%%." % int(status.progress() * 100)
+import requests
+
+
+def download_file_from_google_drive(file_id, dest):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    t = get_confirm_token(response)
+
+    if t:
+        params = {'id': file_id, 'confirm': t}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, dest)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+file_id = '0B6lEo20DNMISIJDSJDVBMENXbkE'
+destination = './data/'
+download_file_from_google_drive(file_id, destination)
---
+++ setup_scripts/setup.sh
@@ -0,0 +1,4 @@
+
+
+#!/bin/sh
+python get_data.py
