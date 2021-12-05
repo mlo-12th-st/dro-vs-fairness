166690a382f35e10e31a4fbc0365fa92c8cf699c
dan11109 <steved7@rpi.edu>
2021-12-04 22:00:10 -0500

    updates

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -5,3 +5,12 @@
 python get_data.py 1nNGRZOl9X4ryeCkEKMBbxNKeCHOp4ShU ../data/celeba/img_align_celeba.zip
 #get the GAN generated fake celebA dataset
 python get_data.py 1XzUvl3q1-cf9M6TaXWyIfK0gJV33lfcH ../data/celeba/fake_images.zip
+#record.zip file
+python get_data.py 12l8MJ8PNCiDCJSoOHezaikm3BN9CgCbk ../FairnessModels/record/record.zip
+
+#models.zip file
+python get_data.py 1Ap4zG6YRJukxjbptaaqdVs0b_fkT9b_G ../models.zip
+
+#celeba.zip
+python get_data.py 14mfX1QdtEbxpBDrrZmy7FDPKEpufD0Eq ../data/celeba.zip
+
