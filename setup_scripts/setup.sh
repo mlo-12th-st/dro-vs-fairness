48c5ee3d0d66e2bea5a6be6eadbb4486b857340d
dan11109 <steved7@rpi.edu>
2021-12-04 19:00:25 -0500

    Getting GAN generated dataset

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -1,4 +1,7 @@
 
 
 #!/bin/sh
-python3 get_data.py 1nNGRZOl9X4ryeCkEKMBbxNKeCHOp4ShU ../data/celeba/img_align_celeba.zip
+#Get the orignial celebA dataset
+python get_data.py 1nNGRZOl9X4ryeCkEKMBbxNKeCHOp4ShU ../data/celeba/img_align_celeba.zip
+#get the GAN generated fake celebA dataset
+python get_data.py 1XzUvl3q1-cf9M6TaXWyIfK0gJV33lfcH ../data/celeba/fake_images.zip
