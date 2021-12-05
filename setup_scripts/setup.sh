abfb941d9d0f144acd78c90ba15a56ba4d930250
dan11109 <steved7@rpi.edu>
2021-12-04 23:44:36 -0500

    final

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -8,9 +8,13 @@ python get_data.py 1XzUvl3q1-cf9M6TaXWyIfK0gJV33lfcH ../data/celeba/fake_images.
 #record.zip file
 python get_data.py 12l8MJ8PNCiDCJSoOHezaikm3BN9CgCbk ../FairnessModels/record/record.zip
 
-#models.zip file
-python get_data.py 1Ap4zG6YRJukxjbptaaqdVs0b_fkT9b_G ../models.zip
-
 #celeba.zip
 python get_data.py 14mfX1QdtEbxpBDrrZmy7FDPKEpufD0Eq ../data/celeba.zip
+
+#models.zip file
+python get_data.py 16ngVpC2o9WiqC_it9on4XuFV5EBmrhKi ../models.tar 1
+
+tar -xzf models.tar
+
+
 
